a3d180db66684bc8626f082226f37d793b04f434
Student3 <student3@NIBMADMIN03s-iMac.local>
2026-06-06 16:37:31 +0530

    initial commit

---
+++ iostutorial/iostutorial/iostutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iostutorialApp.swift
+//  iostutorial
+//
+//  Created by Student3 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iostutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
